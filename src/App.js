f99719566d840057d45ab0afb5bca98794b8f953
Alex Hapgood <ach1@vt.edu>
2020-01-23 14:39:26 -0500

    add content
    add content

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import List from './List'
+import './App.css';
+
+function App (props) {
+        const { store } = props;
+        return (
+            <main className='App'>
+                <header className='App-header'>
+                    <h1>Trelloyes!</h1>
+                </header>
+                <div className='App-list'>
+                    {store.lists.map(list => (
+                        <List
+                            key={list.id}
+                            header={list.header}
+                            cards={list.cardIds.map(id => store.allCards[id])}
+                        />
+                    ))}
+                </div>
+            </main>
+        );
+
+}
+
+export default App;
